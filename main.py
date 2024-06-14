785d682e1fc515672998d6ad4ad6205e103dc575
wadoyoka <122070165+wadoyoka@users.noreply.github.com>
2024-06-14 12:10:29 +0900

    index

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse #インポート
+
+
 app = FastAPI()
 
 
@@ -12,3 +15,17 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
