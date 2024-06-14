6dcb792475dbe5827a4fe52593b750cbc925c790
wadoyoka <122070165+wadoyoka@users.noreply.github.com>
2024-06-14 12:34:17 +0900

    takoyaki

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "takoyaki"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
